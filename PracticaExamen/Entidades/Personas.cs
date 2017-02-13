028913f0dfbc873638b6159c27f5fd413bf501da
Luis Tejada <Luis Tejada@LUISTEJADA-PC>
2017-02-13 09:04:13 -0400

    DataBase

---
+++ PracticaExamen/Entidades/Personas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaExamen.Entidades
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
